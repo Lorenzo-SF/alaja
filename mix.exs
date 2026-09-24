35e35f20983c0364f251bee7f3b2ed3c5e0ccbee
Lorenzo Sánchez Fraile <sanchezfrailelorenzo@gmail.com>
2026-09-24 20:14:23 +0200

    fix(deps): update batamanta to version 3.0.0 and dialyxir to 1.4.8

--- mix.exs
+++ mix.exs
@@ -209,7 +209,7 @@ defmodule Alaja.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:batamanta, "~> 2.0.0", optional: true, runtime: false},
+      {:batamanta, "~> 3.0.0", optional: true, runtime: false},
       {:excoveralls, "~> 0.18", only: :test, runtime: false},
       {:benchee, "~> 1.3", only: :dev}
     ]
